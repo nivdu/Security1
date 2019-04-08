a8c9d784714dc92acac49d761fe1f0dd1723ff7f
nivdu <nivdu@post.bgu.ac.il>
2019-04-08 20:37:33 +0300

    v`

--- src/Decryption.java
+++ src/Decryption.java
@@ -1,8 +1,13 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Decryption implements encryptDecrypt {
 
 
-    public void getKeys(String path2Keys) {
-
+    public void getKeys(String path2Keys) throws IOException {
+        File file = new File(path2Keys);
+        byte[] fileContent = Files.readAllBytes(file.toPath());
     }
 
     public void getFile(String path2File) {
@@ -11,6 +16,11 @@ public class Decryption implements encryptDecrypt {
 
     public void write2Path(String path2Write) {
 
+    }
+
+    private String aesTag(String msg, String k1, String k2, String k3){
+
+        return null;
     }
 
     @Override
